3f2432c2bb2fe82e81dd707df03527c53efa883c
Michael W Chung <dryhslaw@gmail.com>
2018-11-12 14:53:18 -0500

    user testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,35 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save
+    redirect_to "/users/#{user.id}"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :fname, :lname, :birthday)
+  end
 
 end
